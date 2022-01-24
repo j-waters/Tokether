d27d66f76afc0dc1f6bb606767183bee8989032e
j-waters <j-waters@users.noreply.github.com>
2022-01-24 21:02:24 +0000

    deploy: 334dec72fd7e675903aebc18f57adbc9e118bf7d

---
+++ precache-manifest.0a180474c6e3ccbb568bb741b73616b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85734ba172b1d276d557",
+    "url": "/css/app.ec867066.css"
+  },
+  {
+    "revision": "0fb9466684f86392cf72c032c546454c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85734ba172b1d276d557",
+    "url": "/js/app.92c717e7.js"
+  },
+  {
+    "revision": "8d1b680c2d37a90b0248",
+    "url": "/js/chunk-vendors.51ed24bf.js"
+  },
+  {
+    "revision": "315a24e91d0f14766a5e4a57cc530a47",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
