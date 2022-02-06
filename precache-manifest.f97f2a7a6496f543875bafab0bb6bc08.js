681bee1d99df944722c4f72e52d3d40c250b9fc5
j-waters <j-waters@users.noreply.github.com>
2022-02-06 00:51:47 +0000

    deploy: 7484ffb6851b2f351cf68975944370833a8ac85b

---
+++ precache-manifest.f97f2a7a6496f543875bafab0bb6bc08.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf06cff9adf566f63417f23eb6eef54b",
+    "url": "/404.html"
+  },
+  {
+    "revision": "6b4b63a32537ce6f23ea88b870a97b2e",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "c5b42ce446a5f6dc4ff5",
+    "url": "/css/app.9b9289a7.css"
+  },
+  {
+    "revision": "57f943d222667cd66d75",
+    "url": "/css/chunk-vendors.ff2f374f.css"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/fonts/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/fonts/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/fonts/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/fonts/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/img/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "e68387e42f0283c20dae5c80ed6ef74a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5b42ce446a5f6dc4ff5",
+    "url": "/js/app.7ef6fd4a.js"
+  },
+  {
+    "revision": "57f943d222667cd66d75",
+    "url": "/js/chunk-vendors.e8442ce2.js"
+  },
+  {
+    "revision": "315a24e91d0f14766a5e4a57cc530a47",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.958a127fbe76dbdbb1455b95bd828a8d.js"
+  "/precache-manifest.f97f2a7a6496f543875bafab0bb6bc08.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "@tokether/site"});
