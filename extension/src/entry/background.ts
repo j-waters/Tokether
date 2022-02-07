a58ce4c0a2dc897d22a35c7084b7ac59cb9786c5
James Waters <james@jcwaters.co.uk>
2022-02-07 22:36:41 +0000

    Intercept web requests on start
    To try and get the messages in more of an order

--- extension/src/entry/background.ts
+++ extension/src/entry/background.ts
@@ -9,9 +9,9 @@ import {
 } from "@tokether/common";
 import { DEFAULT_STATE } from "@tokether/common/lib/types";
 import state from "@/helpers/state";
-import {ingestVideos} from "@/helpers/ingestVideos";
+import { ingestVideos } from "@/helpers/ingestVideos";
 
-chrome.webRequest.onCompleted.addListener(
+chrome.webRequest.onBeforeRequest.addListener(
   async function (details) {
     if (!(await state.messagesScraping)) return;
 
